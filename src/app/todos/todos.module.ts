e99f8cb9892c851be3a5746bdaf94a59f9f3e425
Caio Santos <caio.santos687@gmail.com>
2019-08-21 22:00:41 -0300

    add header and todos component

--- src/app/todos/todos.module.ts
+++ src/app/todos/todos.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { TodoComponent } from './todo/todo.component';
+import { InputTodoComponent } from './input-todo/input-todo.component';
+import { TodosComponent } from './todos.component';
 
 
 
 @NgModule({
-  declarations: [TodoListComponent, TodoComponent],
+  declarations: [TodoListComponent, TodoComponent, InputTodoComponent, TodosComponent],
   imports: [
     CommonModule
   ]
